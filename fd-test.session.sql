0a6c989b6315a9b73fd8e531ce36880d6a75b4e7
Oleksandr Plokhykh <plohishi.family@gmail.com>
2023-09-14 10:32:40 +0200

    chats

--- fd-test.session.sql
+++ fd-test.session.sql
@@ -1,3 +1,5 @@
+DROP TABLE books;
+
 CREATE TABLE books(
     name varchar(300),
     author varchar(300),
@@ -25,15 +27,13 @@ ALTER TABLE users
 ADD COLUMN weight numeric(5,2) CHECK (weight > 0);
 
 ALTER TABLE users
-DROP COLUMN weight;
-
-
-ALTER TABLE users
 ADD CONSTRAINT "early_date" CHECK (birthday > '1990-01-01');
 
 ALTER TABLE users 
 ADD CONSTRAINT "to_high_user" CHECK (height <= 4.0);
 
+ALTER TABLE users
+DROP COLUMN weight;
 
 ALTER TABLE users
 DROP CONSTRAINT "to_high_user";
@@ -43,16 +43,6 @@ INSERT INTO users (first_name, last_name, email, birthday, gender, height, weigh
 ('Iron', 'Man', 'tony@stark.com', '1991-02-02', 'male', '1.98', '68.4'),
 ('Spider', 'Man', 'spider@man.com', '1998-10-10', 'male', '1.67', '68');
 
-
-DROP TABLE messages;
-
-CREATE TABLE messages (
-id serial PRIMARY KEY,    
-body text NOT NULL CHECK (body != ''),
-author varchar(200) NOT NULL CHECK (author != ''),
-created_at timestamp NOT NULL CHECK (created_at <= current_timestamp) DEFAULT current_timestamp,
-is_read boolean NOT NULL DEFAULT false
-);
 
 INSERT INTO messages VALUES 
 ('hello text', 'test user', NULL, false);
@@ -92,3 +82,48 @@ CREATE TABLE orders_to_products(
     quantity int CHECK (quantity > 0),
     PRIMARY KEY (product_id, order_id)
 );
+
+INSERT INTO orders_to_products (product_id, order_id, quantity) VALUES
+(1, 6, 1),
+(2, 6, 1),
+(3, 6, 1);
+
+
+/*  CHAT  */
+
+
+CREATE TABLE messages (
+id serial PRIMARY KEY,    
+body text NOT NULL CHECK (body != ''),
+author_id int REFERENCES users(id),
+chat_id int REFERENCES chats(id),
+created_at timestamp NOT NULL CHECK (created_at <= current_timestamp) DEFAULT current_timestamp,
+is_read boolean NOT NULL DEFAULT false
+);
+
+
+CREATE TABLE chats (
+    id serial PRIMARY KEY,
+    chat_name varchar(256) NOT NULL CHECK (chat_name != ''),
+    created_at timestamp NOT NULL CHECK (created_at <= current_timestamp) DEFAULT current_timestamp
+);
+
+
+
+CREATE TABLE chats_to_users (
+    chat_id int REFERENCES chats(id),
+    user_id int REFERENCES users(id),
+    PRIMARY KEY (chat_id, user_id)
+);
+
+INSERT INTO chats (chat_name) VALUES
+('First chat');
+
+INSERT INTO chats_to_users VALUES
+(5, 3), (5, 4), (5, 5);
+
+INSERT INTO messages (body, author_id, chat_id) VALUES
+('Heello', 3, 5),
+('Hey', 4, 5),
+('So', 5, 5);
+
